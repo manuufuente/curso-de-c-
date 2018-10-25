9e82011839c89be148bcb65fb90704520e8f675b
manuufuente <Manuu.qac@gmail.com>
2018-10-24 22:05:29 -0300

    clase 3

---
+++ clase3/MiControl/UserControl1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MiControl
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmb_lenguajes = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elegir lenguaje";
+            // 
+            // cmb_lenguajes
+            // 
+            this.cmb_lenguajes.FormattingEnabled = true;
+            this.cmb_lenguajes.Location = new System.Drawing.Point(19, 91);
+            this.cmb_lenguajes.Name = "cmb_lenguajes";
+            this.cmb_lenguajes.Size = new System.Drawing.Size(133, 21);
+            this.cmb_lenguajes.TabIndex = 1;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cmb_lenguajes);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(179, 149);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmb_lenguajes;
+    }
+}
---
+++ clase3/MiControl/UserControl1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiControl
+{
+    public partial class UserControl1: UserControl
+    {
+        public EventHandler cambiocomboselect;
+        public UserControl1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(inicontrol);
+        }
+
+        private void inicontrol(object sender, EventArgs e)
+        {
+            this.bindearcolumnas();
+            cmb_lenguajes.SelectedIndexChanged += new EventHandler(cmbcambio);
+        }
+
+        private void cmbcambio(object sender, EventArgs e)
+        {
+            cambiocomboselect(sender, e); 
+        }
+
+        private void bindearcolumnas()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("textPart");
+            dt.Columns.Add("valuePart");
+
+            dt.Rows.Add("c#", "2");
+            dt.Rows.Add("PHP", "1");
+            dt.Rows.Add("java", "3");
+            dt.Rows.Add("python", "4");
+
+            cmb_lenguajes.DataSource = dt;
+            cmb_lenguajes.DisplayMember = "textPart";
+            cmb_lenguajes.ValueMember = "valuePart";
+        }
+
+
+        public string selectedText { get { return cmb_lenguajes.Text; } }
+        public string selectedvalue { get { return cmb_lenguajes.SelectedValue.ToString(); } }
+    }
+}
+
+   
+
+
